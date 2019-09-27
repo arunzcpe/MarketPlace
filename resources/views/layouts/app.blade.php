0d7ffbf7e94d0e5e37cf9ec56b469b33ca99373e
Khairul Azmi <khairulfs21@gmail.com>
2019-09-27 09:09:59 +0800

    add delete modal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -20,7 +20,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
+    <div>
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -102,5 +102,6 @@
             @yield('content')
         </main>
     </div>
+    @yield('scripts')
 </body>
 </html>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -25,7 +25,7 @@
                                 <td> <span class="badge badge-primary">Sold</span> </td>
                                 <td>
                                     <a href="#" class="btn btn-info btn-sm">Edit</a>
-                                    <a href="#" class="btn btn-danger btn-sm">Delete</a>
+                                    <a href="#" class="btn btn-danger btn-sm  btn-delete">Delete</a>
                                 </td>
                             </tr>
                         </tbody>
@@ -34,5 +34,37 @@
             </div>
         </div>
     </div>
+    <!-- Modal -->
+    <div class="modal fade" id="deleteModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Are you sure you want to delete this product?</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <form action="" method="post" id="form-delete">
+              <div class="modal-body">
+
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-danger">Confirm</button>
+              </div>
+          </form>
+        </div>
+      </div>
+    </div>
 </div>
+@endsection
+@section('scripts')
+    <script type="text/javascript">
+        $(".btn-delete").on( "click", function(e){
+            event.preventDefault();
+            var modal = $('#deleteModel');
+            modal.find("#form-delete").attr('action', $(this).attr('href'));
+            modal.modal('show');
+        });
+    </script>
 @endsection
